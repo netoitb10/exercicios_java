414160fd62610f735082234d679a2fa615522997
Manoel Nascimento <neto.itaituba@yahoo.com.br>
2020-01-22 16:06:31 -0300

    Correção da duração do jogo
    Correção do cálculo da duração do jogo no máximo 24h e no mínimo 1h.

--- exercicios4.java
+++ exercicios4.java
@@ -10,20 +10,22 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+
 /**
  *
  * @author manoel
  */
 public class exercicios4 {
     
-    @SuppressWarnings({"UseSpecificCatch", "ConvertToTryWithResources", "null"})
+    @SuppressWarnings({"ConvertToTryWithResources", "UseSpecificCatch", "null"})
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
         DateFormat formatador = new SimpleDateFormat("HH:mm");
-        Date hora_in = null, hora_fin = null;
+        Date hora_in = null, hora_fin = null, meia_noite = null, hora_min = null ;
+        long ms = 0;
         
-        System.out.println("Este programa calcula a duração de um jogo.");
+        System.out.println("Este programa calcula a duração de um jogo, que tem no mínimo 1h e no máximo 24h");
         
         System.out.println("Insira a hora inicial do jogo (HH:MM Ex.: 12:38):");
         String hora_inicial = sc.nextLine();
@@ -35,20 +37,36 @@ public class exercicios4 {
             //transforma o texto em data
             hora_in = formatador.parse(hora_inicial);
             hora_fin = formatador.parse(hora_final);
+            meia_noite = formatador.parse("23:59:59");
+            hora_min = formatador.parse("01:00:00");
+            
+            //diferença entre tempos (milisegundos)
+            ms = hora_fin.getTime() - hora_in.getTime();
         } catch (Exception e) {
             System.err.print(e);
         }
-        
-        //diferença entre tempos (milisegundos)
-        long ms = hora_fin.getTime() - hora_in.getTime();
+                        
+        if ((hora_fin.getTime() <= meia_noite.getTime()) && (ms > hora_min.getTime())) {
+            
+            DuracaoJogo(ms);
+                        
+        } else {
+            
+            //diferença entre 24:00 e hora inicial (milisegundos)
+            long aux_in = 86400000 - hora_in.getTime();
+            ms = hora_fin.getTime() + aux_in;
+            DuracaoJogo(ms);
+        }    
+       
+        sc.close();
+    }
+
+    private static void DuracaoJogo(long ms) {
         
         //converte milisegundos em horas e minutos
-        @SuppressWarnings("MalformedFormatString")
-        String diferenca = String.format( "%03d:%02d", ms / 3600000, ( ms / 60000 ) % 60 );
+        String diferenca = String.format( "%02d:%02d", ms / 3600000, ( ms / 60000 ) % 60 );
         
         System.out.printf("O jogo durou: %s %n", diferenca);
-                
-        sc.close();
     }
     
 }
